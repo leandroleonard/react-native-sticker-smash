1022e622556baa0568d2a17bf3c302e80375c32a
Leandro <leandroventura@webtagger.com.br>
2025-04-25 19:07:54 -0300

    config tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Home screen</Text>
+      </View>
+    </>
   );
 }
 
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,21 +1,24 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
-export default function About() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Route not found</Text>
-    </View>
-  );
+export default function NotFound() {
+    return (
+        <>
+            <View style={styles.container}>
+                <Link href="/">Go to the Home Screen</Link>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
 });
