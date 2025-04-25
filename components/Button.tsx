1b47574c14d2582da34dea42b9539f913e050244
Leandro <leandroventura@webtagger.com.br>
2025-04-25 19:55:51 -0300

    use an image picker

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,10 +3,11 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
     label: string;
-    theme: 'primary';
+    theme?: 'primary';
+    onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme , onPress}: Props) {
     if (theme === 'primary') {
         return (
             <View style={[
@@ -15,7 +16,7 @@ export default function Button({ label, theme }: Props) {
                     borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18
                 }
             ]}>
-                <Pressable style={[styles.button, {backgroundColor: '#FFF'}]} onPress={() => alert('You pressed a button')}>
+                <Pressable style={[styles.button, {backgroundColor: '#FFF'}]} onPress={onPress}>
                     <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
                     <Text style={[styles.buttonLabel, {color: '#25292e'}]}>{label}</Text>
                 </Pressable>
